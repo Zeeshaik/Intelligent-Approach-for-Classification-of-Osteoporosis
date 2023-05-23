335ef65c2babc09c7992d40ccfb2136420ee15ef
Shaik Zeeshan <zeeshaik1431@gmail.com>
2023-05-24 01:36:30 +0530

    Edited by Zeeshan on front end

--- Flask App/app.py
+++ Flask App/app.py
@@ -22,11 +22,17 @@ def predict(img_path):
 
 def get_precaution(prediction):
     precautions = {
-        'Normal': 'Your Knee is normal, No specific precautions needed.',
-        'Doubtful': 'Consult a healthcare professional for further evaluation.',
-        'Moderate': 'Take necessary precautions and consider medical advice.',
-        'Mild': 'Take necessary precautions and consider medical advice.',
-        'Severe': 'Seek immediate medical attention.'
+        'Normal': ["Maintain a balanced and nutritious diet to support bone health.",
+                   " Engage in regular weight-bearing exercises like walking, jogging, or dancing to strengthen bones.",
+                   " Avoid excessive alcohol consumption and smoking, as they can contribute to bone loss.", 
+                   " Get regular check-ups and bone density tests as recommended by your healthcare provider."],
+        'Doubtful': [" Consult a healthcare professional for further evaluation and diagnosis."," Follow any additional tests or screenings recommended by your healthcare provider."," Maintain a healthy lifestyle with a focus on nutrition, exercise, and overall well-being."],
+        'Moderate': [" Take necessary precautions to prevent falls, such as removing hazards at home, using assistive devices, and ensuring proper lighting."," Follow the recommendations of your healthcare provider regarding medication, supplements, and physical therapy."," Engage in exercises that focus on balance, strength, and flexibility to reduce the risk of fractures."," Consider modifications in daily activities to prevent excessive strain on the bones."],
+        'Mild': [" Take necessary precautions similar to those for the moderate category."," Follow the advice of your healthcare provider regarding lifestyle modifications, medication, and therapeutic interventions."," Engage in exercises that are appropriate for your condition and focus on improving bone strength and flexibility."," Ensure an adequate intake of calcium, vitamin D, and other essential nutrients for bone health."],
+        'Severe': [" Seek immediate medical attention and follow the guidance of healthcare professionals.",
+                   " Adhere strictly to the prescribed treatment plan, including medication, therapy, and lifestyle modifications.",
+                   " Take precautions to minimize the risk of falls and fractures, such as using assistive devices and making necessary home modifications.",
+                   " Engage in physical activities as recommended by your healthcare provider, considering the limitations of your condition."]
     }
     return precautions.get(prediction)
 
