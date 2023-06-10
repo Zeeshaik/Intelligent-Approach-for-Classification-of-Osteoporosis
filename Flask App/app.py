46377b46f91ae2ff4f9809ebb47d5b738da8bd13
Shaik Zeeshan <zeeshaik1431@gmail.com>
2023-06-10 10:35:42 +0530

    Added the user mobile number feature in which the user will get sms meassage for predictiona and precautions

--- Flask App/app.py
+++ Flask App/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask import Flask, render_template, request, send_from_directory
 from sklearn.model_selection import train_test_split
 from PIL import Image
+from twilio.rest import Client
 import numpy as np
 import tensorflow as tf
 import cv2,os
@@ -53,10 +54,24 @@ from keras.utils import np_utils
 new_label=np_utils.to_categorical(label)
 
 
-
-
-
-
+def auth(prediction, precaution, phone):
+    account_sid = 'AC711078708a1823783e0a9693b7d2696f'
+    auth_token = 'c6ff719cc84d30702912a7c87495ce18'
+    number = '+13614597059'
+    phone = "+91" + phone
+    print(phone)
+    dest = phone
+    precautions = ""
+    for i in precaution:
+        precautions += i + "\n"
+    msg_body = "Predicted Category:"+prediction + "\n" +"Precautons:\n"+ precautions
+    
+    client = Client(account_sid, auth_token)
+    msg = client.messages.create(
+        body = msg_body,
+        from_= number,
+        to = dest
+    )
 
 def predict(img_path):
     img = Image.open(img_path).convert('L')  # convert to grayscale
@@ -75,7 +90,7 @@ def get_precaution(prediction):
                    " Engage in regular weight-bearing exercises like walking, jogging, or dancing to strengthen bones.",
                    " Avoid excessive alcohol consumption and smoking, as they can contribute to bone loss.", 
                    " Get regular check-ups and bone density tests as recommended by your healthcare provider."],
-        'Doubtful': [" Consult a healthcare professional for further evaluation and diagnosis."," Follow any additional tests or screenings recommended by your healthcare provider."," Maintain a healthy lifestyle with a focus on nutrition, exercise, and overall well-being."],
+        'Doubtful': [" Eat food which contains Vitamin D."," Follow any additional tests or screenings recommended by your healthcare provider."," Maintain a healthy lifestyle with a focus on nutrition, exercise, and overall well-being."],
         'Moderate': [" Take necessary precautions to prevent falls, such as removing hazards at home, using assistive devices, and ensuring proper lighting."," Follow the recommendations of your healthcare provider regarding medication, supplements, and physical therapy."," Engage in exercises that focus on balance, strength, and flexibility to reduce the risk of fractures."," Consider modifications in daily activities to prevent excessive strain on the bones."],
         'Mild': [" Take necessary precautions similar to those for the moderate category."," Follow the advice of your healthcare provider regarding lifestyle modifications, medication, and therapeutic interventions."," Engage in exercises that are appropriate for your condition and focus on improving bone strength and flexibility."," Ensure an adequate intake of calcium, vitamin D, and other essential nutrients for bone health."],
         'Severe': [" Seek immediate medical attention and follow the guidance of healthcare professionals.",
@@ -96,10 +111,12 @@ def index():
             return "No file selected"
         if file:
             file_path = "C:/Users/zeesh/OneDrive/Documents/Projects/Intelligent-Approach-for-Classification-of-Osteoporosis/Flask App/static/uploads/" + file.filename
+            phone = request.form['phone']
             file.save(file_path)
             prediction = predict(file_path)
             precaution = get_precaution(prediction)
-            
+            print(phone)
+            auth(prediction, precaution, phone)
             # res = prediction + " detected " + precaution
             return render_template("result.html", prediction = prediction, precaution=precaution, image_file=file.filename)
     return render_template("index.html")
